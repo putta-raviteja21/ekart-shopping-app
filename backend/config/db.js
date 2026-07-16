3d25ac1f70f44c9976cdd2273e095b59b91a11e7
putta-raviteja21 <puttaraviteja21@gmail.com>
2026-07-16 17:39:54 +0530

    Prepare backend for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,21 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "T5j1@2120",
-  database: "shopping_app"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
-
-  if(err){
-    console.log(err);
+  if (err) {
+    console.error("MySQL Connection Error:", err);
+  } else {
+    console.log("✅ MySQL Connected");
   }
-
-  else{
-    console.log("MySQL Connected");
-  }
-
 });
 
 module.exports = db;
